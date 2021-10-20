826229a6868ab7bb1cca4ca39b52e780995ecb6f
Santiago Mondragon <santiago.mondragon224@gmail.com>
2021-10-20 16:25:32 -0700

    refactor code

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -5,26 +5,35 @@
 // Print the result to the terminal
 const readline = require('readline-sync');
 
-console.log('Welcome to Calculator!');
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log('What\'s the first number?');
+prompt('Welcome to Calculator!');
+
+prompt('What\'s the first number?');
 let number1 = readline.question();
 
-console.log('What\'s the second number?');
+prompt('What\'s the second number?');
 let number2 = readline.question();
 
-console.log('What operation would you like to perform?\n1) Add 2) Substract 3) Multiply 4) Divide');
+prompt('What operation would you like to perform?\n1) Add 2) Substract 3) Multiply 4) Divide');
 let operation = readline.question();
 
 let output;
-if (operation === '1') { // '1' represents addition
-  output = Number(number1) + Number(number2);
-} else if (operation === '2') { // '2' represents subtraction
-  output = Number(number1) - Number(number2);
-} else if (operation === '3') { // '3' represents multiplication
-  output = Number(number1) * Number(number2);
-} else if (operation === '4') {  // '4' represents division
-  output = Number(number1) / Number(number2);
+switch (operation) {
+  case '1': // '1' represents addition
+    output = Number(number1) + Number(number2);
+    break;
+  case '2': // '2' represents subtraction
+    output = Number(number1) - Number(number2);
+    break;
+  case '3': // '3' represents multiplication
+    output = Number(number1) * Number(number2);
+    break;
+  case '4': // '4' represents division
+    output = Number(number1) / Number(number2);
+    break;
 }
 
-console.log(`The result is: ${output}`);
+prompt(`The result is: ${output}`);
