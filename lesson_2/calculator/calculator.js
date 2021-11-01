72ca6fce84807793383278c80f706f792823744a
Santiago Mondragon <santiago.mondragon224@gmail.com>
2021-11-01 16:20:40 -0700

    refactor prompt function

--- lesson_2/calculator/calculator.js
+++ lesson_2/calculator/calculator.js
@@ -5,8 +5,10 @@
 // Print the result to the terminal
 const readline = require('readline-sync');
 const messages = require('./calculator_messages.json');
+const userLanugage = 'en';
 
-function prompt(message) {
+function prompt(key) {
+  let message = messages[userLanugage][key];
   console.log(`=> ${message}`);
 }
 
@@ -15,32 +17,31 @@ function invalidNumber(number) {
 }
 
 let useCalculator;
-let userLanugage = 'en';
 
-prompt(messages[userLanugage].welcome);
+prompt('welcome');
 
 do {
-  prompt(messages[userLanugage].firstNumber);
+  prompt('firstNumber');
   let number1 = readline.question();
 
   while (invalidNumber(number1)) {
-    prompt(messages[userLanugage].invalidNumber);
+    prompt('invalidNumber');
     number1 = readline.question();
   }
 
-  prompt(messages[userLanugage].secondNumber);
+  prompt('secondNumber');
   let number2 = readline.question();
 
   while (invalidNumber(number2)) {
-    prompt(messages[userLanugage].invalidNumber);
+    prompt('invalidNumber');
     number2 = readline.question();
   }
 
-  prompt(messages[userLanugage].selectOperation);
+  prompt('selectOperation');
   let operation = readline.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(messages[userLanugage].listOptions);
+    prompt('listOptions');
     operation = readline.question();
   }
 
@@ -60,9 +61,9 @@ do {
       break;
   }
 
-  prompt(`The result is: ${output}`);
+  console.log(`=> ${messages[userLanugage].result} ${output}`); // couldn't use prompt because it uses the variable 'output'
 
-  prompt(messages[userLanugage].continue);
+  prompt('continue');
   useCalculator = readline.question();
 
 } while (useCalculator && useCalculator[0].toLowerCase() === 'y');
