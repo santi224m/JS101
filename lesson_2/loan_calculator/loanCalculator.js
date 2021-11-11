c89e96efea9a2a5e3ed119eb3d60075dd6d971f9
Santiago Mondragon <santiago.mondragon224@gmail.com>
2021-11-10 19:55:46 -0800

    finish loanCalculator.js

--- lesson_2/loan_calculator/loanCalculator.js
+++ lesson_2/loan_calculator/loanCalculator.js
@@ -1,3 +1,47 @@
-//  Ask user for the loan amount, APR, and loan duration
-// Calculate monthly interest rate and loan duration in months
-// Use the formula to calculate monthly payment
+const readline = require('readline-sync');
+const messages = require('./messages.json');
+let continueCalculation = true;
+
+let prompt = text => {
+  console.log('=> ' + text);
+};
+
+let invalidNumber = number => {
+  return Number.isNaN(Number(number)) || number.trimStart() === '' || number < 0;
+};
+
+let getInput = (messageQuestion, messageError = 'invalidNumber') => {
+  let userInput;
+  prompt(messages[messageQuestion]);
+  userInput = readline.question().replaceAll(',', '');
+  while (invalidNumber(userInput)) {
+    prompt(messages[messageError]);
+    userInput = readline.question().replaceAll(',', '');
+  }
+
+  return Number(userInput);
+};
+
+prompt(messages.welcome);
+do {
+  let loanAmount = getInput('loanAmount');
+  let apr = getInput('apr') / 100;
+  let loanDurationYears = getInput('loanDurationYears');
+  let loanDurationMonths = getInput('loanDurationMonths');
+  let totalLoanDuration = loanDurationYears + (loanDurationMonths / 12);
+  let monthlyInterestRate = apr / 12;
+  let totalDurationMonths =  totalLoanDuration * 12;
+  let monthlyPayments;
+  if (apr === 0) {
+    monthlyPayments = loanAmount / totalDurationMonths;
+  } else {
+    monthlyPayments = loanAmount *
+              (monthlyInterestRate /
+              (1 - Math.pow((1 + monthlyInterestRate),(-totalDurationMonths))));
+  }
+  prompt(`Your monthly payment is: $${monthlyPayments.toFixed(2)}`);
+  prompt(messages.continueCalculation);
+  continueCalculation = readline.question().toLowerCase()[0];
+} while (continueCalculation && continueCalculation !== 'n');
+
+prompt(messages.endMessage);
