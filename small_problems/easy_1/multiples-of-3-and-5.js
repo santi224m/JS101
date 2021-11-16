a33ef19f383594d1788f3768562961d10d78b7cd
Santiago Mondragon <santiago.mondragon224@gmail.com>
2021-11-15 19:44:32 -0800

    refactor

--- small_problems/easy_1/multiples-of-3-and-5.js
+++ small_problems/easy_1/multiples-of-3-and-5.js
@@ -1,15 +1,16 @@
-// Write a function that computes the sum of all numbers between 1 and some other number, inclusive, that are multiples of 3 or 5.
+// Write a function that computes the sum of all numbers between 1 and
+// some other number, inclusive, that are multiples of 3 or 5
 
 let multisum = (num) => {
   let count = 0;
-  for (let i = 1; i <= num; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      count += i;
+  for (let number = 1; number <= num; number++) {
+    if (number % 3 === 0 || number % 5 === 0) {
+      count += number;
     }
   }
 
   return count;
-}
+};
 
 console.log(multisum(3));
 console.log(multisum(5));
